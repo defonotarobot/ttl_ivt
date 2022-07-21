b63f8040a888ee24ba7b78a4973c6d7c3da5c4c3
Your Namedefonotarobot <thr.leelasithorn@gmail.com>
2022-07-21 15:23:37 +0700

    Added a few lines of comment on the main .py as a guide to skip the exporter

--- export_and_compare.py
+++ export_and_compare.py
@@ -1,14 +1,18 @@
 from comparator import Comparator
 from exporter import Exporter
 
-# Input (lower-case) names of two environments to compare
-source_env = "ps2"
-target_env = "uat"
+# Input (lower-case) names of two environments to compare (do not mix up source/target)
+source_env = "prod"
+target_env = "gpe"
 
-# Export tables from desired environment
+# Export tables from desired environment, comment out if you dont need to export
 exporter = Exporter()
 source_filepath = exporter.export(source_env)
 target_filepath = exporter.export(target_env)
+
+# uncomment these and put path if you dont need to export
+# source_filepath = "../data_prod_21072022/"
+# target_filepath = "../data_gpe_21072022/"
 
 # Compare the exported tables
 comparator = Comparator()
